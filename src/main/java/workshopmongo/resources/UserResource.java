91b1766826df09fbbfb553cc390f4cd77594f552
DAVI SANTOS <davi-rocha2@hotmail.com>
2020-08-13 13:08:55 -0300

    connecting MongoDB with repository and service

--- src/main/java/workshopmongo/resources/UserResource.java
+++ src/main/java/workshopmongo/resources/UserResource.java
@@ -1,29 +1,29 @@
 package workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import workshopmongo.domain.User;
+import workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
 	
 	/* Annotation @GetMapping also works */
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Brown", "maria@email.com");
-		User alex = new User("2", "Alex Green", "alex@email.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
+	
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 		
 	}
---
+++ src/main/java/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import workshopmongo.domain.User;
+import workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+		
+	}
+
+}
