7bb5c1c274248ffd5afe7d4b295a9257b5c024d8
DAVI SANTOS <davi-rocha2@hotmail.com>
2020-08-14 16:10:15 -0300

    Endpoint to get User posts

--- src/main/java/workshopmongo/resources/UserResource.java
+++ src/main/java/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import workshopmongo.domain.Post;
 import workshopmongo.domain.User;
 import workshopmongo.dto.UserDTO;
 import workshopmongo.services.UserService;
@@ -62,4 +63,11 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 }
