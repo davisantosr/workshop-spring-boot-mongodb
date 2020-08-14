794a63b5a505c0507195a31f20907f937da6a714
DAVI SANTOS <davi-rocha2@hotmail.com>
2020-08-14 14:37:46 -0300

    Applying User data (in post) with DTO

--- src/main/java/workshopmongo/config/Instantiation.java
+++ src/main/java/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import workshopmongo.domain.Post;
 import workshopmongo.domain.User;
+import workshopmongo.dto.AuthorDTO;
 import workshopmongo.repository.PostRepository;
 import workshopmongo.repository.UserRepository;
 
@@ -35,14 +36,13 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para SP", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei Feliz hoje!", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para SP", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei Feliz hoje!", new AuthorDTO(maria));
+		
+	
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
-		
 		
 	}
 
